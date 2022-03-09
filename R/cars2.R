f2baf1a8657fe49f87d335201370e8d9a1d7b158
jonahseif <72671007+jonahseif@users.noreply.github.com>
2022-03-09 10:26:35 -0800

    added cars2 dataset and description file

---
+++ R/cars2.R
@@ -0,0 +1,12 @@
+#' Speed and Stopping Distances of Cars
+#'
+#' The data give the speed of cars and the distances taken to stop.
+#'
+#' @format A data frame with 50 observations on 2 variables
+#' /itemize{
+#' /item [,1] speed numeric (mph)
+#' /item [,2] distance numeric (ft)
+#' }
+#' @source Ezekiel, M. (1930) Methods of Correlation Analysis. Wiley.
+#'
+"cars2"
